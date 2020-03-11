cb6cb412eea88d5ad16dd87daed678928826caa5
pedfav <pedfav@gmail.com>
2020-03-11 01:45:11 -0300

    taks by department grid

---
+++ taskit-backend/src/main/java/br/com/pedfav/taskitbackend/exception/CouldNotUpdateTaskException.java
@@ -0,0 +1,4 @@
+package br.com.pedfav.taskitbackend.exception;
+
+public class CouldNotUpdateTaskException {
+}
---
+++ taskit-backend/src/main/java/br/com/pedfav/taskitbackend/http/datacontracts/TaskCreatedDataContract.java
@@ -0,0 +1,4 @@
+package br.com.pedfav.taskitbackend.http.datacontracts;
+
+public class TaskCreatedDataContract {
+}
